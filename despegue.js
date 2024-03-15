022df49658c2c865dd1d3537b1f43a5140d6b42d
Vainilatea <124099125+Vainilatea@users.noreply.github.com>
2024-03-15 11:40:41 +0100

    Add files via upload

---
+++ despegue.js
@@ -0,0 +1,7 @@
+var contador                // declaramos la variable contador
+contador = 1000               // asignamos 10 al contador
+while(contador > 0) {       // e.control, comparación
+    console.log(contador)   // funcion salida
+    contador--              // operacion (resta)
+}
+console.log("DESPEGUE")     // funcion salida
---
+++ despegueedit.js
@@ -0,0 +1,8 @@
+var contador                // declaramos la variable contador
+contador = 1000               // asignamos 10 al contador
+while(contador > 0) {       // e.control, comparación
+     
+    contador--
+    console.log(contador)                
+}
+console.log("DESPEGUE")     // funcion salida
